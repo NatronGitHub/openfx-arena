4b17e4f4125b1dd6bc89d8cacfeea16fe432071c
Ole-André Rodlie <olear@dracolinux.org>
2015-04-24 09:09:28 +0200

    removed pasture

--- Magick/MagickEdge.h
+++
@@ -1,8 +0,0 @@
-#ifndef __Io__MAGICK_EDGE__
-#define __Io__MAGICK_EDGE__
-
-#include "ofxsImageEffect.h"
-
-void getMagickEdgePluginID(OFX::PluginFactoryArray &ids);
-
-#endif /* __Io__MAGICK_EDGE__*/
