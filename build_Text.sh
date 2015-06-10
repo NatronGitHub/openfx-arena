f413ed93b1c09f41610bea1ec09df0a9594d29ff
Ole-André Rodlie <olear@dracolinux.org>
2015-06-10 17:21:46 +0200

    Text: bump version

--- build_Text.sh
+++ build_Text.sh
@@ -4,6 +4,6 @@ if [ "$NOCLEAN" == "1" ]; then
 else
   DOCLEAN=1
 fi
-CLEAN=$DOCLEAN MAGICK_LEGACY=1 MAGICK_MOD=1 PACKAGE=Text VERSION=3.2 sh deploy.sh || exit 1
+CLEAN=$DOCLEAN MAGICK_LEGACY=1 MAGICK_MOD=1 PACKAGE=Text VERSION=4.0 sh deploy.sh || exit 1
 echo "Text.ofx DONE!!!"
 
