166938140d457dae3efa17a22e36a76260612f52
Ole-André Rodlie <olear@dracolinux.org>
2015-06-28 08:55:25 +0200

    ReadSVG: misc fixes

--- ReadSVG/ReadSVG.cpp
+++ ReadSVG/ReadSVG.cpp
@@ -48,17 +48,19 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define kPluginGrouping "Image/Readers"
 #define kPluginIdentifier "net.fxarena.openfx.ReadSVG"
 #define kPluginVersionMajor 1
-#define kPluginVersionMinor 0
+#define kPluginVersionMinor 2
 
 #define kParamDpi "dpi"
 #define kParamDpiLabel "DPI"
 #define kParamDpiHint "Dots-per-inch"
-#define kParamDpiDefault 0
+#define kParamDpiDefault 72
 
 #define kSupportsRGBA true
 #define kSupportsRGB false
 #define kSupportsAlpha false
 #define kSupportsTiles false
+
+#define DEBUG_MAGICK false
 
 class ReadSVGPlugin : public GenericReaderPlugin
 {
@@ -67,17 +69,28 @@ public:
     virtual ~ReadSVGPlugin();
 private:
     virtual bool isVideoStream(const std::string& /*filename*/) OVERRIDE FINAL { return false; }
+    virtual void changedParam(const OFX::InstanceChangedArgs &args, const std::string &paramName) OVERRIDE FINAL;
     virtual void decode(const std::string& filename, OfxTime time, const OfxRectI& renderWindow, float *pixelData, const OfxRectI& bounds, OFX::PixelComponentEnum pixelComponents, int pixelComponentCount, int rowBytes) OVERRIDE FINAL;
     virtual bool getFrameBounds(const std::string& filename, OfxTime time, OfxRectI *bounds, double *par, std::string *error) OVERRIDE FINAL;
+    virtual void restoreState(const std::string& filename) OVERRIDE FINAL;
     virtual void onInputFileChanged(const std::string& newFile, OFX::PreMultiplicationEnum *premult, OFX::PixelComponentEnum *components, int *componentCount) OVERRIDE FINAL;
     OFX::IntParam *dpi_;
+    bool hasRSVG_;
+    int width_;
+    int height_;
 };
 
 ReadSVGPlugin::ReadSVGPlugin(OfxImageEffectHandle handle)
 : GenericReaderPlugin(handle, kSupportsRGBA, kSupportsRGB, kSupportsAlpha, kSupportsTiles, false)
-, dpi_(0)
+,dpi_(0)
+,hasRSVG_(false)
+,width_(0)
+,height_(0)
 {
     Magick::InitializeMagick(NULL);
+    std::string delegates = MagickCore::GetMagickDelegates();
+    if (delegates.find("rsvg") != std::string::npos)
+        hasRSVG_ = true;
 
     dpi_ = fetchIntParam(kParamDpi);
     assert(dpi_);
@@ -90,23 +103,32 @@ ReadSVGPlugin::~ReadSVGPlugin()
 void
 ReadSVGPlugin::decode(const std::string& filename,
                       OfxTime time,
-                      const OfxRectI& renderWindow,
+                      const OfxRectI& /*renderWindow*/,
                       float *pixelData,
                       const OfxRectI& bounds,
-                      OFX::PixelComponentEnum pixelComponents,
-                      int pixelComponentCount,
-                      int rowBytes)
+                      OFX::PixelComponentEnum /*pixelComponents*/,
+                      int /*pixelComponentCount*/,
+                      int /*rowBytes*/)
 {
+    #ifdef DEBUG
+    std::cout << "decode ..." << std::endl;
+    #endif
+    if (!hasRSVG_)
+        setPersistentMessage(OFX::Message::eMessageError, "", "librsvg missing");
     int dpi = 0;
     dpi_->getValueAtTime(time, dpi);
     Magick::Image image;
-    if (dpi>0) {
-        image.resolutionUnits(Magick::PixelsPerInchResolution);
-        image.density(Magick::Geometry(dpi,dpi));
-    }
+    #ifdef DEBUG
+    image.debug(DEBUG_MAGICK);
+    #endif
+    image.resolutionUnits(Magick::PixelsPerInchResolution);
+    image.density(Magick::Geometry(dpi,dpi));
     image.read(filename);
     if (image.columns()>0 && image.rows()>0) {
         Magick::Image container(Magick::Geometry(bounds.x2,bounds.y2),Magick::Color("rgba(0,0,0,0)"));
+        #ifdef DEBUG
+        container.debug(DEBUG_MAGICK);
+        #endif
         container.composite(image,0,0,Magick::OverCompositeOp);
         container.flip();
         container.write(0,0,bounds.x2,bounds.y2,"RGBA",Magick::FloatPixel,pixelData);
@@ -117,40 +139,120 @@ ReadSVGPlugin::decode(const std::string& filename,
     }
 }
 
-bool ReadSVGPlugin::getFrameBounds(const std::string& filename,
-                              OfxTime time,
+bool ReadSVGPlugin::getFrameBounds(const std::string& /*filename*/,
+                              OfxTime /*time*/,
                               OfxRectI *bounds,
                               double *par,
-                              std::string *error)
+                              std::string* /*error*/)
 {
-    int dpi = 0;
-    dpi_->getValueAtTime(time, dpi);
-    Magick::Image image;
-    if (dpi>0) {
-        image.resolutionUnits(Magick::PixelsPerInchResolution);
-        image.density(Magick::Geometry(dpi,dpi));
+    #ifdef DEBUG
+    std::cout << "getFrameBounds ..." << std::endl;
+    #endif
+    if (width_>0 && height_>0) {
+        bounds->x1 = 0;
+        bounds->x2 = width_;
+        bounds->y1 = 0;
+        bounds->y2 = height_;
+        *par = 1.0;
     }
+    return true;
+}
+
+void ReadSVGPlugin::restoreState(const std::string& filename)
+{
+    #ifdef DEBUG
+    std::cout << "restoreState ..." << std::endl;
+    #endif
+    Magick::Image image;
+    #ifdef DEBUG
+    image.debug(DEBUG_MAGICK);
+    #endif
+    int dpi;
+    dpi_->getValue(dpi);
+    image.resolutionUnits(Magick::PixelsPerInchResolution);
+    image.density(Magick::Geometry(dpi,dpi));
     image.read(filename);
     if (image.columns()>0 && image.rows()>0) {
-        bounds->x1 = 0;
-        bounds->x2 = image.columns();
-        bounds->y1 = 0;
-        bounds->y2 = image.rows();
-        *par = 1.0;
-        return true;
+        width_ = image.columns();
+        height_ = image.rows();
     }
     else {
         setPersistentMessage(OFX::Message::eMessageError, "", "Unable to read image");
         OFX::throwSuiteStatusException(kOfxStatErrFormat);
     }
-    return false;
 }
 
-void ReadSVGPlugin::onInputFileChanged(const std::string& /*newFile*/,
-                                  OFX::PreMultiplicationEnum *premult,
-                                  OFX::PixelComponentEnum *components,int *componentCount)
+void ReadSVGPlugin::changedParam(const OFX::InstanceChangedArgs &args, const std::string &paramName)
 {
+    #ifdef DEBUG
+    std::cout << "changedParam ..." << std::endl;
+    #endif
+    if (paramName == kParamDpi) {
+        int dpi;
+        std::string imageFile;
+        _fileParam->getValue(imageFile);
+        dpi_->getValue(dpi);
+        #ifdef DEBUG
+        std::cout << "changed dpi to " << dpi << std::endl;
+        #endif
+        Magick::Image image;
+        #ifdef DEBUG
+        image.debug(DEBUG_MAGICK);
+        #endif
+        image.resolutionUnits(Magick::PixelsPerInchResolution);
+        image.density(Magick::Geometry(dpi,dpi));
+        image.read(imageFile);
+        if (image.columns()>0 && image.rows()>0) {
+            width_ = image.columns();
+            height_ = image.rows();
+        }
+    }
+    else {
+        GenericReaderPlugin::changedParam(args,paramName);
+    }
+}
+
+void ReadSVGPlugin::onInputFileChanged(const std::string& newFile,
+                                  OFX::PreMultiplicationEnum *premult,
+                                  OFX::PixelComponentEnum *components,int */*componentCount*/)
+{
+    #ifdef DEBUG
+    std::cout << "onInputFileChanged ..." << std::endl;
+    #endif
     assert(premult && components);
+    Magick::Image image;
+    #ifdef DEBUG
+    image.debug(DEBUG_MAGICK);
+    #endif
+    int dpi;
+    dpi_->getValue(dpi);
+    image.resolutionUnits(Magick::PixelsPerInchResolution);
+    image.density(Magick::Geometry(dpi,dpi));
+    image.read(newFile);
+    if (image.columns()>0 && image.rows()>0) {
+        width_ = image.columns();
+        height_ = image.rows();
+        # ifdef OFX_IO_USING_OCIO
+        switch(image.colorSpace()) {
+        case Magick::RGBColorspace:
+            _ocio->setInputColorspace("sRGB");
+            break;
+        case Magick::sRGBColorspace:
+            _ocio->setInputColorspace("sRGB");
+            break;
+        case Magick::scRGBColorspace:
+            _ocio->setInputColorspace("sRGB");
+            break;
+        default:
+            _ocio->setInputColorspace("Linear");
+            break;
+        }
+        # endif // OFX_IO_USING_OCIO
+    }
+    else {
+        setPersistentMessage(OFX::Message::eMessageError, "", "Unable to read image");
+        OFX::throwSuiteStatusException(kOfxStatErrFormat);
+    }
     *components = OFX::ePixelComponentRGBA;
     *premult = OFX::eImageOpaque;
 }
@@ -173,7 +275,7 @@ void ReadSVGPluginFactory::describe(OFX::ImageEffectDescriptor &desc)
 
     std::string magickV = MagickCore::GetMagickVersion(NULL);
     std::string delegates = MagickCore::GetMagickDelegates();
-    desc.setPluginDescription("Read SVG image formats.\n\nWritten by Ole-André Rodlie <olear@fxarena.net>\n\n Powered by "+magickV+"\n\nFeatures: "+delegates);
+    desc.setPluginDescription("Read SVG image format.\n\nWritten by Ole-André Rodlie <olear@fxarena.net>\n\nPowered by "+magickV+"\n\nFeatures: "+delegates);
 }
 
 /** @brief The describe in context function, passed a plugin descriptor and a context */
@@ -185,7 +287,7 @@ void ReadSVGPluginFactory::describeInContext(OFX::ImageEffectDescriptor &desc, C
         param->setLabel(kParamDpiLabel);
         param->setHint(kParamDpiHint);
         param->setRange(0, 10000);
-        param->setDisplayRange(0, 4000);
+        param->setDisplayRange(0, 500);
         param->setDefault(kParamDpiDefault);
         page->addChild(*param);
     }
