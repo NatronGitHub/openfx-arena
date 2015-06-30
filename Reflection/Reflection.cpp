dde2a66931f2c457deaa09e58249791699f857a6
Ole-André Rodlie <olear@dracolinux.org>
2015-07-01 01:30:26 +0200

    Reflection: merged Mirror, issue #118

--- Reflection/Reflection.cpp
+++ Reflection/Reflection.cpp
@@ -42,23 +42,33 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define kPluginName "Reflection"
 #define kPluginGrouping "Transform"
 #define kPluginIdentifier "net.fxarena.openfx.Reflection"
-#define kPluginVersionMajor 2
-#define kPluginVersionMinor 4
+#define kPluginVersionMajor 3
+#define kPluginVersionMinor 0
 
 #define kParamSpace "spacing"
-#define kParamSpaceLabel "Spacing"
+#define kParamSpaceLabel "Reflection spacing"
 #define kParamSpaceHint "Space between image and reflection"
 #define kParamSpaceDefault 0
 
 #define kParamOffset "offset"
-#define kParamOffsetLabel "Offset"
-#define kParamOffsetHint "Mirror offset"
+#define kParamOffsetLabel "Reflection offset"
+#define kParamOffsetHint "Reflection offset"
 #define kParamOffsetDefault 0
 
 #define kParamMatte "matte"
 #define kParamMatteLabel "Matte"
 #define kParamMatteHint "Merge Alpha before applying effect"
 #define kParamMatteDefault false
+
+#define kParamReflection "reflection"
+#define kParamReflectionLabel "Reflection"
+#define kParamReflectionHint "Apply reflection"
+#define kParamReflectionDefault true
+
+#define kParamMirror "mirror"
+#define kParamMirrorLabel "Mirror"
+#define kParamMirrorHint "Select mirror type"
+#define kParamMirrorDefault 0
 
 #define kSupportsTiles 0
 #define kSupportsMultiResolution 1
@@ -82,6 +92,8 @@ private:
     OFX::IntParam *spacing_;
     OFX::IntParam *offset_;
     OFX::BooleanParam *matte_;
+    OFX::BooleanParam *reflection_;
+    OFX::ChoiceParam *mirror_;
 };
 
 ReflectionPlugin::ReflectionPlugin(OfxImageEffectHandle handle)
@@ -100,8 +112,10 @@ ReflectionPlugin::ReflectionPlugin(OfxImageEffectHandle handle)
     spacing_ = fetchIntParam(kParamSpace);
     offset_ = fetchIntParam(kParamOffset);
     matte_ = fetchBooleanParam(kParamMatte);
+    reflection_ = fetchBooleanParam(kParamReflection);
+    mirror_ = fetchChoiceParam(kParamMirror);
 
-    assert(spacing_ && offset_ && matte_);
+    assert(spacing_ && offset_ && matte_ && mirror_ && reflection_);
 }
 
 ReflectionPlugin::~ReflectionPlugin()
@@ -186,17 +200,26 @@ void ReflectionPlugin::render(const OFX::RenderArguments &args)
     // get params
     int spacing = 0;
     int offset = 0;
+    int mirror = 0;
     bool matte = false;
+    bool reflection = false;
+    mirror_->getValueAtTime(args.time, mirror);
     spacing_->getValueAtTime(args.time, spacing);
     offset_->getValueAtTime(args.time, offset);
     matte_->getValueAtTime(args.time, matte);
+    reflection_->getValueAtTime(args.time, reflection);
 
     // setup
     int srcWidth = srcRod.x2-srcRod.x1;
     int srcHeight = srcRod.y2-srcRod.y1;
+    int mirrorWidth = srcWidth/2;
     int mirrorHeight = srcHeight/2;
     Magick::Image image;
+    Magick::Image image0;
     Magick::Image image1;
+    Magick::Image image2;
+    Magick::Image image3;
+    Magick::Image image4;
 
     // read source image
     Magick::Image container(Magick::Geometry(srcWidth,srcHeight),Magick::Color("rgba(0,0,0,0)"));
@@ -208,28 +231,127 @@ void ReflectionPlugin::render(const OFX::RenderArguments &args)
         image.matte(true);
     }
 
-    // proc images
-    image1 = image;
-    image1.flip();
-    spacing = std::floor(spacing * args.renderScale.x + 0.5);
-    offset = std::floor(offset * args.renderScale.x + 0.5);
-    image1.crop(Magick::Geometry(srcWidth,mirrorHeight-offset,0,offset+offset));
-    image.crop(Magick::Geometry(srcWidth,mirrorHeight+offset,0,mirrorHeight-offset));
-
-    // apply mask
-    if (maskClip_ && maskClip_->isConnected()) {
-        int maskWidth = maskRod.x2-maskRod.x1;
-        int maskHeight = maskRod.y2-maskRod.y1;
-        if (maskWidth>0&&maskHeight>0) {
-            Magick::Image mask(maskWidth,maskHeight,"A",Magick::FloatPixel,(float*)maskImg->getPixelData());
-            //mask.negate();
-            image1.composite(mask,0,0,Magick::CopyOpacityCompositeOp);
-        }
+    // proc mirror
+    Magick::Image mirrorContainer(Magick::Geometry(srcWidth,srcHeight),Magick::Color("rgba(0,0,0,0)"));
+    switch(mirror) {
+    case 1: // North
+        image1 = image;
+        image1.flip();
+        image.crop(Magick::Geometry(srcWidth,mirrorHeight,0,mirrorHeight));
+        image1.crop(Magick::Geometry(srcWidth,mirrorHeight,0,0));
+        mirrorContainer.composite(image,0,mirrorHeight,Magick::OverCompositeOp);
+        mirrorContainer.composite(image1,0,0,Magick::OverCompositeOp);
+        break;
+    case 2: // South
+        image1 = image;
+        image.flip();
+        image.crop(Magick::Geometry(srcWidth,mirrorHeight,0,mirrorHeight));
+        image1.crop(Magick::Geometry(srcWidth,mirrorHeight,0,0));
+        mirrorContainer.composite(image,0,mirrorHeight,Magick::OverCompositeOp);
+        mirrorContainer.composite(image1,0,0,Magick::OverCompositeOp);
+        break;
+    case 3: // East
+        image1 = image;
+        image1.flop();
+        image.crop(Magick::Geometry(mirrorWidth,srcHeight,mirrorWidth,0));
+        image1.crop(Magick::Geometry(mirrorWidth,srcHeight,0,0));
+        mirrorContainer.composite(image,mirrorWidth,0,Magick::OverCompositeOp);
+        mirrorContainer.composite(image1,0,0,Magick::OverCompositeOp);
+        break;
+    case 4: // West
+        image1 = image;
+        image.flop();
+        image.crop(Magick::Geometry(mirrorWidth,srcHeight,mirrorWidth,0));
+        image1.crop(Magick::Geometry(mirrorWidth,srcHeight,0,0));
+        mirrorContainer.composite(image,mirrorWidth,0,Magick::OverCompositeOp);
+        mirrorContainer.composite(image1,0,0,Magick::OverCompositeOp);
+        break;
+    case 5: // NorthWest
+        image1 = image;
+        image1.crop(Magick::Geometry(mirrorWidth,mirrorHeight,0,mirrorHeight));
+        image2 = image1;
+        image2.flop();
+        image3 = image2;
+        image3.flip();
+        image4 = image3;
+        image4.flop();
+        break;
+    case 6: // NorthEast
+        image1 = image;
+        image1.crop(Magick::Geometry(mirrorWidth,mirrorHeight,mirrorWidth,mirrorHeight));
+        image1.flop();
+        image2 = image1;
+        image2.flop();
+        image3 = image2;
+        image3.flip();
+        image4 = image3;
+        image4.flop();
+        break;
+    case 7: // SouthWest
+        image1 = image;
+        image1.crop(Magick::Geometry(mirrorWidth,mirrorHeight,0,0));
+        image1.flip();
+        image2 = image1;
+        image2.flop();
+        image3 = image2;
+        image3.flip();
+        image4 = image3;
+        image4.flop();
+        break;
+    case 8: // SouthEast
+        image1 = image;
+        image1.crop(Magick::Geometry(mirrorWidth,mirrorHeight,mirrorWidth,0));
+        image1.flop();
+        image1.flip();
+        image2 = image1;
+        image2.flop();
+        image3 = image2;
+        image3.flip();
+        image4 = image3;
+        image4.flop();
+        break;
+    case 9: // flip
+        image.flip();
+        break;
+    case 10: // flop
+        image.flop();
+        break;
+    case 11: //flip+flop
+        image.flip();
+        image.flop();
+        break;
+    default:
+        // none
+        break;
     }
-
-    // comp
-    container.composite(image1,0,-spacing,Magick::OverCompositeOp);
-    container.composite(image,0,mirrorHeight-offset,Magick::OverCompositeOp);
+    if (mirror>4&&mirror<9) {
+        mirrorContainer.composite(image1,0,mirrorHeight,Magick::OverCompositeOp);
+        mirrorContainer.composite(image2,mirrorWidth,mirrorHeight,Magick::OverCompositeOp);
+        mirrorContainer.composite(image3,mirrorWidth,0,Magick::OverCompositeOp);
+        mirrorContainer.composite(image4,0,0,Magick::OverCompositeOp);
+    }
+    if (mirror>0&&mirror<9)
+        image=mirrorContainer;
+    if (reflection) {
+        image0 = image;
+        image0.flip();
+        spacing = std::floor(spacing * args.renderScale.x + 0.5);
+        offset = std::floor(offset * args.renderScale.x + 0.5);
+        image0.crop(Magick::Geometry(srcWidth,mirrorHeight-offset,0,offset+offset));
+        image.crop(Magick::Geometry(srcWidth,mirrorHeight+offset,0,mirrorHeight-offset));
+        if (maskClip_ && maskClip_->isConnected()) {
+            int maskWidth = maskRod.x2-maskRod.x1;
+            int maskHeight = maskRod.y2-maskRod.y1;
+            if (maskWidth>0&&maskHeight>0) {
+                Magick::Image mask(maskWidth,maskHeight,"A",Magick::FloatPixel,(float*)maskImg->getPixelData());
+                image0.composite(mask,0,0,Magick::CopyOpacityCompositeOp);
+            }
+        }
+        container.composite(image0,0,-spacing,Magick::OverCompositeOp);
+        container.composite(image,0,mirrorHeight-offset,Magick::OverCompositeOp);
+    }
+    else
+        container=image;
 
     // return image
     if (dstClip_ && dstClip_->isConnected() && srcClip_ && srcClip_->isConnected())
@@ -260,8 +382,7 @@ void ReflectionPluginFactory::describe(OFX::ImageEffectDescriptor &desc)
     desc.setLabel(kPluginName);
     desc.setPluginGrouping(kPluginGrouping);
     std::string magickV = MagickCore::GetMagickVersion(NULL);
-    std::string delegates = MagickCore::GetMagickDelegates();
-    desc.setPluginDescription("Mirror/Reflection filter for Natron.\n\nWritten by Ole-André Rodlie <olear@fxarena.net>\n\n Powered by "+magickV+"\n\nFeatures: "+delegates);
+    desc.setPluginDescription("Mirror/Reflection filter for Natron.\n\nWritten by Ole-André Rodlie <olear@fxarena.net>\n\nPowered by "+magickV);
 
     // add the supported contexts
     desc.addSupportedContext(eContextGeneral);
@@ -321,10 +442,38 @@ void ReflectionPluginFactory::describeInContext(OFX::ImageEffectDescriptor &desc
         page->addChild(*param);
     }
     {
+        BooleanParamDescriptor *param = desc.defineBooleanParam(kParamReflection);
+        param->setLabel(kParamReflectionLabel);
+        param->setHint(kParamReflectionHint);
+        param->setDefault(kParamReflectionDefault);
+        param->setAnimates(true);
+        page->addChild(*param);
+    }
+    {
         BooleanParamDescriptor *param = desc.defineBooleanParam(kParamMatte);
         param->setLabel(kParamMatteLabel);
         param->setHint(kParamMatteHint);
         param->setDefault(kParamMatteDefault);
+        param->setAnimates(true);
+        page->addChild(*param);
+    }
+    {
+        ChoiceParamDescriptor *param = desc.defineChoiceParam(kParamMirror);
+        param->setLabel(kParamMirrorLabel);
+        param->setHint(kParamMirrorHint);
+        param->appendOption("Undefined");
+        param->appendOption("North");
+        param->appendOption("South");
+        param->appendOption("East");
+        param->appendOption("West");
+        param->appendOption("NorthWest");
+        param->appendOption("NorthEast");
+        param->appendOption("SouthWest");
+        param->appendOption("SouthEast");
+        param->appendOption("Flip");
+        param->appendOption("Flop");
+        param->appendOption("Flip+Flop");
+        param->setDefault(kParamMirrorDefault);
         param->setAnimates(true);
         page->addChild(*param);
     }
