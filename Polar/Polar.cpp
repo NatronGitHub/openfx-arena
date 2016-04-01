bfa5605ba57bb9754eb68e307e4eafdef90b8c12
Ole-André Rodlie <olear@dracolinux.org>
2016-04-01 23:53:03 +0200

    Polar: make OpenMP a param, set default to false

--- Polar/Polar.cpp
+++ Polar/Polar.cpp
@@ -19,7 +19,7 @@
 #define kPluginGrouping "Extra/Distort"
 #define kPluginIdentifier "net.fxarena.openfx.Polar"
 #define kPluginVersionMajor 4
-#define kPluginVersionMinor 2
+#define kPluginVersionMinor 3
 
 #define kParamVPixel "pixel"
 #define kParamVPixelLabel "Virtual Pixel"
@@ -46,6 +46,11 @@
 #define kParamMatteHint "Merge Alpha before applying effect"
 #define kParamMatteDefault false
 
+#define kParamOpenMP "openmp"
+#define kParamOpenMPLabel "OpenMP"
+#define kParamOpenMPHint "Enable/Disable OpenMP support. This will enable the plugin to use as many threads as allowed by host. Note that this plugin is known to be unstable with this settings enabled, use at own risk."
+#define kParamOpenMPDefault false
+
 #define kSupportsTiles 0
 #define kSupportsMultiResolution 1
 #define kSupportsRenderScale 1
@@ -53,6 +58,8 @@
 #define kHostFrameThreading false
 
 using namespace OFX;
+
+static bool _hasOpenMP = false;
 
 class PolarPlugin : public OFX::ImageEffect
 {
@@ -69,6 +76,7 @@ private:
     OFX::BooleanParam *dePolar_;
     OFX::BooleanParam *matte_;
     OFX::DoubleParam *polarRotate_;
+    OFX::BooleanParam *enableOpenMP_;
 };
 
 PolarPlugin::PolarPlugin(OfxImageEffectHandle handle)
@@ -87,8 +95,9 @@ PolarPlugin::PolarPlugin(OfxImageEffectHandle handle)
     dePolar_ = fetchBooleanParam(kParamDePolar);
     matte_ = fetchBooleanParam(kParamMatte);
     polarRotate_ = fetchDoubleParam(kParamPolarRotate);
+    enableOpenMP_ = fetchBooleanParam(kParamOpenMP);
 
-    assert(vpixel_ && polarFlip_ && dePolar_ && matte_ && polarRotate_);
+    assert(vpixel_ && polarFlip_ && dePolar_ && matte_ && polarRotate_ && enableOpenMP_);
 }
 
 PolarPlugin::~PolarPlugin()
@@ -172,25 +181,28 @@ void PolarPlugin::render(const OFX::RenderArguments &args)
     bool polarFlip = false;
     bool dePolar = false;
     bool matte = false;
+    bool enableOpenMP = false;
     vpixel_->getValueAtTime(args.time, vpixel);
     polarFlip_->getValueAtTime(args.time, polarFlip);
     dePolar_->getValueAtTime(args.time, dePolar);
     matte_->getValueAtTime(args.time, matte);
     polarRotate_->getValueAtTime(args.time, polarRotate);
+    enableOpenMP_->getValueAtTime(args.time, enableOpenMP);
 
     // setup
     int width = srcRod.x2-srcRod.x1;
     int height = srcRod.y2-srcRod.y1;
 
-    // Set max threads allowed by host
-    unsigned int threads = 0;
-    threads = OFX::MultiThread::getNumCPUs();
-    if (threads>0) {
-        Magick::ResourceLimits::thread(threads);
-        #ifdef DEBUG
-        std::cout << "Setting max threads to " << threads << std::endl;
-        #endif
-    }
+    // OpenMP
+    unsigned int threads = 1;
+    if (_hasOpenMP && enableOpenMP)
+        threads = OFX::MultiThread::getNumCPUs();
+
+    Magick::ResourceLimits::thread(threads);
+
+#ifdef DEBUG
+    std::cout << "Polar threads: " << threads << std::endl;
+#endif
 
     // read image
     Magick::Image image(Magick::Geometry(width,height),Magick::Color("rgba(0,0,0,0)"));
@@ -351,6 +363,11 @@ void PolarPluginFactory::describe(OFX::ImageEffectDescriptor &desc)
 /** @brief The describe in context function, passed a plugin descriptor and a context */
 void PolarPluginFactory::describeInContext(OFX::ImageEffectDescriptor &desc, ContextEnum /*context*/)
 {
+
+    std::string features = MagickCore::GetMagickFeatures();
+    if (features.find("OpenMP") != std::string::npos)
+        _hasOpenMP = true;
+
     // create the mandated source clip
     ClipDescriptor *srcClip = desc.defineClip(kOfxImageEffectSimpleSourceClipName);
     srcClip->addSupportedComponent(ePixelComponentRGBA);
@@ -420,6 +437,18 @@ void PolarPluginFactory::describeInContext(OFX::ImageEffectDescriptor &desc, Con
         param->appendOption("White");
         param->setDefault(kParamVPixelDefault);
         param->setAnimates(true);
+        param->setLayoutHint(OFX::eLayoutHintDivider);
+        page->addChild(*param);
+    }
+    {
+        BooleanParamDescriptor *param = desc.defineBooleanParam(kParamOpenMP);
+        param->setLabel(kParamOpenMPLabel);
+        param->setHint(kParamOpenMPHint);
+        param->setDefault(kParamOpenMPDefault);
+        param->setAnimates(false);
+        if (!_hasOpenMP)
+            param->setEnabled(false);
+        param->setLayoutHint(OFX::eLayoutHintDivider);
         page->addChild(*param);
     }
 }
