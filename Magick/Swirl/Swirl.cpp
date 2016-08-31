c4e6b8946a646a9c05cb8d32af582822b4dead7a
Ole-André Rodlie <olear@dracolinux.org>
2016-08-31 23:44:17 +0200

    Swirl: rewrite using MagickPlugin

---
+++ Magick/Swirl/Swirl.cpp
@@ -0,0 +1,109 @@
+/*
+ * This file is part of openfx-arena <https://github.com/olear/openfx-arena>,
+ * Copyright (C) 2016 INRIA
+ *
+ * openfx-arena is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as published
+ * by the Free Software Foundation.
+ *
+ * openfx-arena is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with openfx-arena.  If not, see <http://www.gnu.org/licenses/gpl-2.0.html>
+*/
+
+#include "MagickPlugin.h"
+
+using namespace OFX;
+OFXS_NAMESPACE_ANONYMOUS_ENTER
+
+#define kPluginName "Swirl"
+#define kPluginGrouping "Extra/Distort"
+#define kPluginIdentifier "net.fxarena.openfx.Swirl"
+#define kPluginDescription "Swirl effect using ImageMagick."
+#define kPluginVersionMajor 2
+#define kPluginVersionMinor 0
+
+#define kSupportsTiles 0
+#define kSupportsMultiResolution 1
+#define kSupportsRenderScale 1
+#define kRenderThreadSafety eRenderFullySafe
+#define kHostFrameThreading false
+#define kHostMasking true
+#define kHostMixing true
+
+#define kParamSwirl "amount"
+#define kParamSwirlLabel "Amount"
+#define kParamSwirlHint "Swirl amount."
+#define kParamSwirlDefault 60
+
+class SwirlPlugin
+    : public MagickPluginHelper<kSupportsRenderScale>
+{
+public:
+    SwirlPlugin(OfxImageEffectHandle handle)
+        : MagickPluginHelper<kSupportsRenderScale>(handle)
+        , _swirl(0)
+    {
+        _swirl = fetchDoubleParam(kParamSwirl);
+        assert(_swirl);
+    }
+
+    virtual void render(const OFX::RenderArguments &args, Magick::Image &image) OVERRIDE FINAL
+    {
+        double amount;
+        _swirl->getValueAtTime(args.time, amount);
+        image.swirl(amount);
+    }
+private:
+    DoubleParam *_swirl;
+};
+
+mDeclarePluginFactory(SwirlPluginFactory, {}, {});
+
+void SwirlPluginFactory::describe(ImageEffectDescriptor &desc)
+{
+    desc.setLabel(kPluginName);
+    desc.setPluginGrouping(kPluginGrouping);
+    desc.setPluginDescription(kPluginDescription);
+    desc.addSupportedContext(eContextGeneral);
+    desc.addSupportedContext(eContextFilter);
+    desc.addSupportedBitDepth(eBitDepthFloat);
+    desc.setSupportsTiles(kSupportsTiles);
+    desc.setSupportsMultiResolution(kSupportsMultiResolution);
+    desc.setRenderThreadSafety(kRenderThreadSafety);
+    desc.setHostFrameThreading(kHostFrameThreading);
+    desc.setHostMaskingEnabled(kHostMasking);
+    desc.setHostMixingEnabled(kHostMixing);
+}
+
+void SwirlPluginFactory::describeInContext(ImageEffectDescriptor &desc, ContextEnum context)
+{
+    OFX::PageParamDescriptor *page = SwirlPlugin::describeInContextBegin(desc, context);
+    {
+        DoubleParamDescriptor *param = desc.defineDoubleParam(kParamSwirl);
+        param->setLabel(kParamSwirlLabel);
+        param->setHint(kParamSwirlHint);
+        param->setRange(-360, 360);
+        param->setDisplayRange(-360, 360);
+        param->setDefault(kParamSwirlDefault);
+        if (page) {
+            page->addChild(*param);
+        }
+    }
+    SwirlPlugin::describeInContextEnd(desc, context, page);
+}
+
+OFX::ImageEffect*
+SwirlPluginFactory::createInstance(OfxImageEffectHandle handle, ContextEnum /*context*/)
+{
+    return new SwirlPlugin(handle);
+}
+
+static SwirlPluginFactory p(kPluginIdentifier, kPluginVersionMajor, kPluginVersionMinor);
+mRegisterPluginFactoryInstance(p)
+
+OFXS_NAMESPACE_ANONYMOUS_EXIT
